a18520147c8afee41e7c536477e717603fd5c7b4
Luciano Murruni <luciano.murruni@gmail.com>
2016-01-25 16:55:12 +0100

    add header information

--- header.php
+++ header.php
@@ -1,77 +1,98 @@
+<?php
+/**
+ * The header for WFE Education.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package w4e-education
+ */
+global $woocommerce;
+?>
 <!doctype html>
-
 <!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
 <!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
 <!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?>><!--<![endif]-->
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <?php // force Internet Explorer to use the latest rendering engine available ?>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title><?php wp_title(''); ?></title>
+  <?php // mobile meta (hooray!) ?>
+  <meta name="HandheldFriendly" content="True">
+  <meta name="MobileOptimized" content="320">
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
+  <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon.png">
+  <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+  <!--[if IE]>
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+  <![endif]-->
+  <?php // or, set /favicon.ico for IE10 win ?>
+  <meta name="msapplication-TileColor" content="#f01d4f">
+  <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+  <meta name="theme-color" content="#121212">
+  <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+  <?php // wordpress head functions ?>
+  <?php wp_head(); ?>
+  <?php // end of wordpress head ?>
+  <?php // drop Google Analytics Here ?>
+  <?php // end analytics ?>
+</head>
+<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
+  <div id="container">
+    <header class="row row-full row-debug header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
+      <div id="base-header" class="base">
+        <div class="wrap cf ">
+          <div class="row nowrap row-align-middle" >
+            <div class="gr-adapt">italiano</div>
+            <div class="gr-grow"></div>
+            <div class="gr-adapt">
+                <?php if ( is_user_logged_in() ) { ?>
+                    <div class="site-header-right-link"><a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>" title="<?php _e('My Account','oceanic'); ?>"><?php _e('My Account','oceanic'); ?></a></div>
+                <?php } else { ?>
+                    <div class="site-header-right-link"><a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>" title="<?php _e('Login','oceanic'); ?>"><?php _e('Sign In / Register','oceanic'); ?></a></div>
+                <?php } ?>
+                <div class="header-cart">
+                    <a class="header-cart-contents" href="<?php echo $woocommerce->cart->get_cart_url(); ?>" title="<?php _e('View your shopping cart', 'oceanic'); ?>">
+                        <span class="header-cart-amount">
+                            <?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'oceanic'), $woocommerce->cart->cart_contents_count);?> - <?php echo $woocommerce->cart->get_cart_total(); ?>
+                        </span>
+                        <span class="header-cart-checkout<?php echo ( $woocommerce->cart->cart_contents_count > 0 ) ? ' cart-has-items' : ''; ?>">
+                            <span><?php _e('Checkout', 'oceanic'); ?></span> <i class="fa fa-shopping-cart"></i>
+                        </span>
+                    </a>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div> <!-- base header -->
+      <div id="inner-header" class="wrap cf">
+        <?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
+          <div class="row nowrap wrap@mobile row-align-middle" >
+            <div class="gr-adapt">=</div>
+            <div class="gr-grow">
+              <p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization">
+                <a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a>
+                <span class="description"><?php  bloginfo('description'); ?></span>
+              </p>
 
-	<head>
-		<meta charset="utf-8">
-
-		<?php // force Internet Explorer to use the latest rendering engine available ?>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-		<title><?php wp_title(''); ?></title>
-
-		<?php // mobile meta (hooray!) ?>
-		<meta name="HandheldFriendly" content="True">
-		<meta name="MobileOptimized" content="320">
-		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-
-		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
-		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon.png">
-		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
-		<!--[if IE]>
-			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-		<![endif]-->
-		<?php // or, set /favicon.ico for IE10 win ?>
-		<meta name="msapplication-TileColor" content="#f01d4f">
-		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
-            <meta name="theme-color" content="#121212">
-
-		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-
-		<?php // wordpress head functions ?>
-		<?php wp_head(); ?>
-		<?php // end of wordpress head ?>
-
-		<?php // drop Google Analytics Here ?>
-		<?php // end analytics ?>
-
-	</head>
-
-	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
-
-		<div id="container">
-
-			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
-
-				<div id="inner-header" class="wrap cf">
-
-					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
-
-					<?php // if you'd like to use the site description you can un-comment it below ?>
-					<?php // bloginfo('description'); ?>
-
-
-					<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-						<?php wp_nav_menu(array(
-    					         'container' => false,                           // remove nav container
-    					         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
-    					         'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-    					         'menu_class' => 'nav top-nav cf',               // adding custom nav class
-    					         'theme_location' => 'main-nav',                 // where it's located in the theme
-    					         'before' => '',                                 // before the menu
-        			               'after' => '',                                  // after the menu
-        			               'link_before' => '',                            // before each link
-        			               'link_after' => '',                             // after each link
-        			               'depth' => 0,                                   // limit the depth of the nav
-    					         'fallback_cb' => ''                             // fallback function (if there is one)
-						)); ?>
-
-					</nav>
-
-				</div>
-
-			</header>
+              <nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+                <?php wp_nav_menu(array(
+                'container' => false,                           // remove nav container
+                'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+                'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
+                'menu_class' => 'nav top-nav cf',               // adding custom nav class
+                'theme_location' => 'main-nav',                 // where it's located in the theme
+                'before' => '',                                 // before the menu
+                'after' => '',                                  // after the menu
+                'link_before' => '',                            // before each link
+                'link_after' => '',                             // after each link
+                'depth' => 0,                                   // limit the depth of the nav
+                'fallback_cb' => ''                             // fallback function (if there is one)
+                )); ?>
+              </nav>
+          </div>
+          <div class="gr-adapt"></div>
+        </div>
+      </header>
--- library/js/scripts.js
+++ library/js/scripts.js
@@ -12,37 +12,34 @@
  * not required. It's your world baby, you can do whatever you want.
 */
 
-
 /*
  * Get Viewport Dimensions
  * returns object with viewport dimensions to match css in width and height properties
  * ( source: http://andylangton.co.uk/blog/development/get-viewport-size-width-and-height-javascript )
 */
 function updateViewportDimensions() {
-	var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
-	return { width:x,height:y };
+  var w = window,d = document,e = d.documentElement,g = d.getElementsByTagName('body')[0],x = w.innerWidth || e.clientWidth || g.clientWidth,y = w.innerHeight || e.clientHeight || g.clientHeight;
+  return {width:x,height:y};
 }
 // setting the viewport width
 var viewport = updateViewportDimensions();
-
 
 /*
  * Throttle Resize-triggered Events
  * Wrap your actions in this function to throttle the frequency of firing them off, for better performance, esp. on mobile.
  * ( source: http://stackoverflow.com/questions/2854407/javascript-jquery-window-resize-how-to-fire-after-the-resize-is-completed )
 */
-var waitForFinalEvent = (function () {
-	var timers = {};
-	return function (callback, ms, uniqueId) {
-		if (!uniqueId) { uniqueId = "Don't call this twice without a uniqueId"; }
-		if (timers[uniqueId]) { clearTimeout (timers[uniqueId]); }
-		timers[uniqueId] = setTimeout(callback, ms);
-	};
+var waitForFinalEvent = (function() {
+  var timers = {};
+  return function(callback, ms, uniqueId) {
+    if (!uniqueId) { uniqueId = 'Don\'t call this twice without a uniqueId'; }
+    if (timers[uniqueId]) { clearTimeout (timers[uniqueId]); }
+    timers[uniqueId] = setTimeout(callback, ms);
+  };
 })();
 
 // how long to wait before deciding the resize has stopped, in ms. Around 50-100 should work ok.
 var timeToWaitForLast = 100;
-
 
 /*
  * Here's an example so you can see how we're using the above function
@@ -66,8 +63,8 @@ var timeToWaitForLast = 100;
  *    // if we're on the home page, we wait the set amount (in function above) then fire the function
  *    if( is_home ) { waitForFinalEvent( function() {
  *
- *	// update the viewport, in case the window size has changed
- *	viewport = updateViewportDimensions();
+ *  // update the viewport, in case the window size has changed
+ *  viewport = updateViewportDimensions();
  *
  *      // if we're above or equal to 768 fire this off
  *      if( viewport.width >= 768 ) {
@@ -98,23 +95,30 @@ function loadGravatars() {
   viewport = updateViewportDimensions();
   // if the viewport is tablet or larger, we load in the gravatars
   if (viewport.width >= 768) {
-  jQuery('.comment img[data-gravatar]').each(function(){
+    jQuery('.comment img[data-gravatar]').each(function() {
     jQuery(this).attr('src',jQuery(this).attr('data-gravatar'));
   });
-	}
+  }
 } // end function
-
 
 /*
  * Put all your regular jQuery in here.
 */
 jQuery(document).ready(function($) {
 
+  $('#main').slick({
+    autoplay:true,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+
   /*
    * Let's fire off the gravatar function
    * You can remove this if you don't need it
   */
   loadGravatars();
-
 
 }); /* end of as page load scripts */
